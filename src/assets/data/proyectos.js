10d31214dee90118b2fdc3e9927519d4beea789a
ElCabal <cabalholguinlufe@gmail.com>
2022-04-25 09:49:02 -0500

    resolving bug images test 2

--- src/assets/data/proyectos.js
+++ src/assets/data/proyectos.js
@@ -2,14 +2,14 @@ const projects = [
   {
     id: 0,
     title: "CRUDO",
-    image: "/public/assets/images/proyecto-crudo.png",
+    image: "/assets/images/proyecto-crudo.png",
     deploy: "https://crudos-app.netlify.app/",
     repo: "https://github.com/ElCabal/CRUDo-",
   },
   {
     id: 1,
     title: "TO DO APP",
-    image: "/public/assets/images/proyecto-todoapp.png",
+    image: "/assets/images/proyecto-todoapp.png",
     deploy: "https://todolistappfront.netlify.app/?",
     repo: "https://github.com/ElCabal/frontend_todo_app",
   },
